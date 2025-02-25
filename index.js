0a852ea74e5cae856bbc8b1d94e616065e04b138
khaledsaifulla010 <khaledsaifulla010@gmail.com>
2025-02-25 11:19:23 +0600

    connect with mongodb

--- index.js
+++ index.js
@@ -2,12 +2,31 @@ const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
 const app = express();
+const { MongoClient, ServerApiVersion } = require("mongodb");
 const port = process.env.PORT || 5000;
 
 // Middlewares //
 
 app.use(cors());
 app.use(express.json());
+
+// Connect With MongoDB //
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.ugbxhsw.mongodb.net/?appName=Cluster0`;
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+  } finally {
+  }
+}
+run().catch(console.dir);
 
 // Server Running //
 
