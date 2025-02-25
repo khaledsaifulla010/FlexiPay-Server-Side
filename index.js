fea7ebfc285ba19fa781f4062f97cd4762ad3dec
khaledsaifulla010 <khaledsaifulla010@gmail.com>
2025-02-25 15:39:15 +0600

    get individual user data using get method

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
 const app = express();
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const port = process.env.PORT || 5000;
 
 // Middlewares //
@@ -25,6 +25,15 @@ async function run() {
   try {
     const sendMoneyCollections = client.db("FlexiPay").collection("send-money");
     const usersCollections = client.db("FlexiPay").collection("users");
+
+    //GET USER INDIVIDUAL DATA //
+    app.get("/users", async (req, res) => {
+      const email = req.query.email;
+      const query = { email: email };
+      const result = await usersCollections.findOne(query);
+      res.send(result);
+    });
+
     // POST A NEW USER //
     app.post("/users", async (req, res) => {
       const user = req.body;
@@ -41,9 +50,25 @@ async function run() {
     // POST A SEND MONEY //
 
     app.post("/sendMoney", async (req, res) => {
-      const query = req.body;
-      const result = await sendMoneyCollections.insertOne(query);
-      res.send(result);
+      const { senderId, amount } = req.body;
+
+      const result = await sendMoneyCollections.insertOne(req.body);
+      const sender = await usersCollections.findOne({
+        _id: new ObjectId(senderId),
+      });
+      const transactionFee = amount > 100 ? 5 : 0;
+      const totalDeduction = amount + transactionFee;
+
+      const updatedBalance = sender.myBalance - totalDeduction;
+      const updateResult = await usersCollections.updateOne(
+        { _id: new ObjectId(senderId) },
+        { $set: { myBalance: updatedBalance } }
+      );
+      if (updateResult.modifiedCount > 0) {
+        res.send({ success: true, newBalance: updatedBalance });
+      } else {
+        res.status(500).send({ error: "Balance update failed!" });
+      }
     });
   } finally {
   }
